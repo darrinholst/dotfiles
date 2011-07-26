310f3925b115e52c4ef66fcb29fb0aaae5326332
Darrin Holst <darrinholst@gmail.com>
2011-07-25 20:00:02 -0500

    janus is getting out of control...specify the plugins I actually want

--- .janus.rake
+++ .janus.rake
@@ -1,2 +1,17 @@
+plugins_to_keep = [
+  "ack.vim",
+  "command_t",
+  "cucumber",
+  "irblack",
+  "javascript",
+  "nerdtree",
+  "nerdcommenter",
+  "supertab",
+  "unimpaired"
+]
+
+(Rake::Task[:default].prerequisites - plugins_to_keep).each{|plugin| skip_vim_plugin(plugin)}
+
 vim_plugin_task "bufexplorer", "https://github.com/vim-scripts/bufexplorer.zip.git"
 vim_plugin_task "tabular", "https://github.com/godlygeek/tabular.git"
+
--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -12,7 +12,7 @@ export RUBYOPT="rubygems"
 export EDITOR=mvim
 export GEM_OPEN_EDITOR=mvim
 export MAVEN_OPTS="-d32 -client -Xmx1024m" # -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=4000"
-export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
+export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
 export JAVA_OPTS="-d32 -client -Xms64m -Xmx256m -XX:MaxPermSize=128m"
 export JRUBY_OPTS="--1.9"
 
