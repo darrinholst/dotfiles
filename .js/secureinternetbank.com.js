0bb9d747cfbcb6baebc5f9692bdadd1c2a778d57
Darrin Holst <darrinholst@gmail.com>
2013-01-03 08:00:41 -0600

    remove old .js script

--- .js/secureinternetbank.com.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  setTimeout(function() {$(".AcctListData").show();}, 2000)
-
-  $("#DisplayPassword").change(function() {
-    setTimeout(function() {$("#Submit").trigger("click");}, 100);
-  });
-});
