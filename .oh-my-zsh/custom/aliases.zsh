617d6e09d1b8e06891d8bcfbca9271310d1deae8
Darrin Holst <darrinholst@gmail.com>
2017-11-02 17:16:14 -0500

    add ssh config and make tmux use utf8 by default

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,6 +1,6 @@
 alias listening='lsof -i -P | grep -i listen'
 alias vim=/Applications/MacVim.app/Contents/MacOS/Vim
-alias tmux='tmux -2'
+alias tmux='tmux -2 -u'
 alias git=hub
 alias gpf='git push --force-with-lease'
 alias run='yarn run -s'
