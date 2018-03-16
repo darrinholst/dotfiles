1573b4e96f1d369ecbc37260d33d56727f1664e9
Darrin Holst <darrinholst@gmail.com>
2018-03-16 06:56:51 -0500

    exports

--- .oh-my-zsh/custom/exports.zsh
+++ .oh-my-zsh/custom/exports.zsh
@@ -12,21 +12,21 @@ export ARCHFLAGS='-arch x86_64'
 
 export RUBYOPT="rubygems"
 export EDITOR=/Applications/MacVim.app/Contents/MacOS/Vim
-export GOPATH=~/Projects/go
 export GIT_EDITOR=/Applications/MacVim.app/Contents/MacOS/Vim
 export GEM_OPEN_EDITOR=/Applications/MacVim.app/Contents/MacOS/Vim
+export NODE_HOME="/usr/local/opt/node@8"
 export JAVA_HOME="`/usr/libexec/java_home -v '1.8*'`"
 export JAVA_OPTS="-client -Xms64m -Xmx1024m"
 export MAVEN_OPTS="-client -Xms64m -Xmx1024m"
 export JRUBY_OPTS="--1.9"
 
+export PATH=$NODE_HOME/bin:$PATH
 export PATH=$JAVA_HOME/bin:$PATH
 export PATH=/usr/bin:$PATH
 export PATH=/opt/local/bin:$PATH
 export PATH=/usr/local/sbin:$PATH
 export PATH=/usr/local/bin:$PATH
 export PATH=./node_modules/.bin:$PATH
-export PATH=$GOPATH/bin:$PATH
 export PATH=~/.bin:$PATH
 export PATH=./bin:$PATH
 export PATH=./script:$PATH
