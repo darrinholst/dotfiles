60369a5441dd63ae45c8876c8d4cec6b76518d06
Darrin Holst <darrinholst@gmail.com>
2022-08-20 12:30:36 -0500

    get eslint fixall on save working

--- .config/nvim/lua/my/javascripting.lua
+++
@@ -1,5 +0,0 @@
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = { '*.tsx', '*.ts', '*.jsx', '*.js' },
-  command = 'silent! EslintFixAll',
-  group = vim.api.nvim_create_augroup('MyAutocmdsJavaScripFormatting', {}),
-})
