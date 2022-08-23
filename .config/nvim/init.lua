4f8e0c6981ebc7bf31d562fa886b909faf6bc37c
Darrin Holst <darrinholst@gmail.com>
2022-08-22 19:34:44 -0500

    moar stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,37 @@
 vim.cmd('set runtimepath^=~/.vim runtimepath+=~/.vim/after')
 vim.cmd('let &packpath = &runtimepath')
+
+vim.g.mapleader       = ","
+vim.opt.number        = true --  show line numbers
+vim.opt.laststatus    = 3 --     show the nvim global status line
+vim.opt.cursorline    = true --  highlight current line
+vim.opt.vb            = true --  no beeping
+vim.opt.scrolloff     = 5 --     Start scrolling 5 lines away from margins
+vim.opt.sidescrolloff = 10 --    Start scrolling 10 columns away from margins
+vim.opt.signcolumn    = "yes"
+vim.opt.timeoutlen    = 1000
+vim.opt.ttimeoutlen   = 100
+vim.opt.updatetime    = 300 --   Smaller updatetime for CursorHold & CursorHoldI
+vim.opt.ignorecase    = true --  ignore case
+vim.opt.smartcase     = true --  don't ignore case if there's an uppercase character
+vim.opt.wrap          = false -- no line wrapping by default
+vim.opt.tabstop       = 2 --     tab == 2 spaces
+vim.opt.shiftwidth    = 2 --     autoindent == 2 spaces
+vim.opt.softtabstop   = 2 --     don't use <Tab>s
+vim.opt.expandtab     = true --  spaces, not tabs
+
+vim.g.python_host_prog  = "/usr/bin/python"
+vim.g.python3_host_prog = "/usr/bin/python3"
+vim.g.node_host_prog    = "~/.nodenv/shims/node"
+
+vim.opt.guifont = "Fura Code Retina Nerd Font Complete:h17"
+vim.opt.termguicolors = true
+
 vim.cmd("source ~/.vimrc")
 
 require("my.cmp")
 require("my.diagnostics")
+require("my.folding")
 require("my.formatter")
 require("my.icons")
 require("my.lsp")
--- .config/nvim/lua/my/lsp.lua
+++ .config/nvim/lua/my/lsp.lua
@@ -1,6 +1,6 @@
 local on_attach = function(client, bufnr)
   vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = true,
+    virtual_text = false,
     signs = true,
   })
 
