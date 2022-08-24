026273ac4c6c4c9dacbe653915a34befd53a97ab
Darrin Holst <darrinholst@gmail.com>
2022-08-24 06:31:30 -0500

    folding and spelling

--- .config/nvim/lua/my/folding.lua
+++ .config/nvim/lua/my/folding.lua
@@ -1,9 +1,16 @@
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.wo.foldmethod = "indent"
+
+-- Treesitter is folding automatically after reading the buffer somehow
+-- vim.wo.foldmethod = "expr"
+-- vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
 
 local group = vim.api.nvim_create_augroup("OpenEm", { clear = true })
 
 vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, {
   group = group,
-  command = "normal zR"
+  callback = function()
+    vim.schedule(function()
+      vim.cmd("normal zR")
+    end)
+  end,
 })
