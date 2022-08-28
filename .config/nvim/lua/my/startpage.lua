092dd26744f7922dbf9db8e6699cefc0fd979ba6
Darrin Holst <darrinholst@gmail.com>
2022-08-28 08:40:24 -0500

    Breaking up the legacy vimrc

--- .config/nvim/lua/my/startpage.lua
+++ .config/nvim/lua/my/startpage.lua
@@ -1,1 +1,9 @@
 vim.g.startify_change_to_vcs_root = 1
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "startify",
+
+  callback = function()
+    vim.keymap.set("", "r", "<cmd>Startify<cr>", { buffer = 0 })
+  end
+})
