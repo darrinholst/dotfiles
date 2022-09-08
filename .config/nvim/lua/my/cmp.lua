48ebd0522e012ad37e29165b203c9bc575c79678
Darrin Holst <darrinholst@gmail.com>
2022-09-08 11:52:13 -0500

    snippets

--- .config/nvim/lua/my/cmp.lua
+++ .config/nvim/lua/my/cmp.lua
@@ -3,59 +3,66 @@ vim.opt.shortmess:append("c")
 
 local cmp = require("cmp")
 local lspkind = require("lspkind")
+local cmp_ultisnips_mappings = require("cmp_nvim_ultisnips.mappings")
 
 lspkind.init()
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-		end,
-	},
+  snippet = {
+    expand = function(args)
+      vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
 
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
 
-	formatting = {
-		format = lspkind.cmp_format({
-			with_text = true,
-			menu = {
-				buffer = "[buf]",
-				nvim_lsp = "[lsp]",
-				nvim_lua = "[api]",
-				path = "[path]",
-				ultisnips = "[snip]",
-			},
-		}),
-	},
+  formatting = {
+    format = lspkind.cmp_format({
+      with_text = true,
+      menu = {
+        buffer = "[buf]",
+        nvim_lsp = "[lsp]",
+        nvim_lua = "[api]",
+        path = "[path]",
+        ultisnips = "[snip]",
+      },
+    }),
+  },
 
-	mapping = cmp.mapping.preset.insert({
-		["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	}),
+  mapping = cmp.mapping.preset.insert({
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<Tab>"] = cmp.mapping(
+      function(fallback) cmp_ultisnips_mappings.expand_or_jump_forwards(fallback) end,
+      { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
+    ),
+    ["<S-Tab>"] = cmp.mapping(
+      function(fallback) cmp_ultisnips_mappings.jump_backwards(fallback) end,
+      { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
+    ),
+  }),
 
-	sources = cmp.config.sources({
-		{ name = "ultisnips" },
-		{ name = "nvim_lsp" },
-		{ name = "nvim_lua" },
-		{ name = "path" },
-		{ name = "buffer", keyword_length = 5 },
-	}),
+  sources = cmp.config.sources({
+    { name = "ultisnips" },
+    { name = "nvim_lsp" },
+    { name = "nvim_lua" },
+    { name = "path" },
+    { name = "buffer", keyword_length = 5 },
+  }),
 
-	experimental = {
-		ghost_text = false,
-		native_menu = false,
-	},
+  experimental = {
+    ghost_text = false,
+    native_menu = false,
+  },
 })
 
 cmp.setup.cmdline(":", {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
 })
