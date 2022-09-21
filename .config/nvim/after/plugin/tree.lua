1dbb9d19b38e097017f81f3abcb2da47df8ab739
Darrin Holst <darrinholst@gmail.com>
2022-09-21 07:13:52 -0500

    nvim tree grep at current directory

--- .config/nvim/after/plugin/tree.lua
+++ .config/nvim/after/plugin/tree.lua
@@ -1,15 +1,17 @@
+local function grep_at_current_tree_node()
+  local node = require("nvim-tree.lib").get_node_at_cursor()
+  if not node then return end
+  require("telescope.builtin").live_grep({ search_dirs = { node.absolute_path } })
+end
+
 require("nvim-tree").setup({
   create_in_closed_folder = true,
-  git = {
-    enable = true,
-    ignore = false,
+  git = { enable = true, ignore = false },
+  view = {
+    mappings = {
+      list = {
+        { key = { "g" }, cb = grep_at_current_tree_node }
+      }
+    }
   },
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "NvimTree",
-
-  callback = function()
-    vim.diagnostic.disable(0)
-  end
 })
