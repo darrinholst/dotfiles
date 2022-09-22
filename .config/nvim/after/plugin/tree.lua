7da892e75bad14338f638cec7c0642586c360e2d
Darrin Holst <darrinholst@gmail.com>
2022-09-21 19:24:57 -0500

    Fix nvim-tree grep at current node

--- .config/nvim/after/plugin/tree.lua
+++ .config/nvim/after/plugin/tree.lua
@@ -1,16 +1,12 @@
-local function grep_at_current_tree_node()
-  local node = require("nvim-tree.lib").get_node_at_cursor()
-  if not node then return end
-  require("telescope.builtin").live_grep({ search_dirs = { node.absolute_path } })
-end
-
 require("nvim-tree").setup({
   create_in_closed_folder = true,
   git = { enable = true, ignore = false },
   view = {
     mappings = {
       list = {
-        { key = { "g" }, cb = grep_at_current_tree_node }
+        { key = { "<C-g>" }, action = "grep here", action_cb = function(node)
+          require("telescope.builtin").live_grep({ search_dirs = { node.absolute_path } })
+        end }
       }
     }
   },
