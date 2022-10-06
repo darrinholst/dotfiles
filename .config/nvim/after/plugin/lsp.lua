d961b28dd043e13fd57cc92b6faa1b83e656c668
Darrin Holst <darrinholst@gmail.com>
2022-10-05 19:44:33 -0500

    hi

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -53,7 +53,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.terraform_fmt,
     null_ls.builtins.diagnostics.cspell.with({
-      -- filetypes = { "javascript", "javascriptreact" },
+      filetypes = { "javascript", "javascriptreact" },
       diagnostics_postprocess = function(diagnostic)
         diagnostic.severity = vim.diagnostic.severity["HINT"]
       end,
