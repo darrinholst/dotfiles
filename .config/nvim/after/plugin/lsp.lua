4dc1a452eca69a46e1f2f44d91eb44517a6f3b18
Darrin Holst <darrinholst@gmail.com>
2023-01-21 15:58:41 -0600

    vimming

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -54,9 +54,13 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
   sources = {
+    null_ls.builtins.formatting.codespell,
+    null_ls.builtins.formatting.xmlformat,
+    null_ls.builtins.formatting.fixjson,
     null_ls.builtins.formatting.terraform_fmt,
+    null_ls.builtins.code_actions.cspell,
     null_ls.builtins.diagnostics.cspell.with({
-      filetypes = { "javascript", "javascriptreact" },
+      -- filetypes = { "javascript", "javascriptreact" },
       diagnostics_postprocess = function(diagnostic)
         diagnostic.severity = vim.diagnostic.severity["HINT"]
       end,
