bc41f604e4d87f832a58d1ce4b81cb5926dd56ec
Darrin Holst <darrinholst@gmail.com>
2023-02-11 12:19:04 -0600

    🧹

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,56 +1,57 @@
 -- vim.lsp.set_log_level "debug"
 -- require("vim.lsp.log").set_format_func(vim.inspect)
 
-require("mason").setup {}
+require('mason').setup {}
 
-require("mason-lspconfig").setup {
+require('mason-lspconfig').setup {
   ensure_installed = {
     -- "bash_debug_server",
     -- "node_debug2_adapter",
     -- "xmlformatter",
-    "bashls",
-    "cucumber_language_server",
-    "eslint",
-    "jsonls",
-    "lemminx",
-    "sumneko_lua",
-    "r_language_server",
-    "tsserver",
-    "yamlls",
+    'bashls',
+    'cucumber_language_server',
+    'eslint',
+    'jsonls',
+    'lemminx',
+    'sumneko_lua',
+    'r_language_server',
+    'tsserver',
+    'yamlls',
   },
 }
 
-local lspconfig = require("lspconfig")
+local lspconfig = require('lspconfig')
 
 local on_attach = function(client, bufnr)
-  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = false,
-    signs = true,
-  })
+  vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+      virtual_text = false,
+      signs = true,
+    })
 
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
   local disableDiagnostics = function()
     vim.diagnostic.disable(bufnr)
   end
 
-  vim.keymap.set("n", "<space>x", disableDiagnostics, bufopts)
-  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
-  vim.keymap.set("n", "<f2>", vim.lsp.buf.rename, bufopts)
-  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>x', disableDiagnostics, bufopts)
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'S', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<f2>', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 
-  if client.supports_method("textDocument/formatting") then
-    vim.keymap.set("", "<C-f>", vim.lsp.buf.format, bufopts)
-    vim.keymap.set("i", "<C-f>", vim.lsp.buf.format, bufopts)
+  if client.supports_method('textDocument/formatting') then
+    vim.keymap.set('', '<C-f>', vim.lsp.buf.format, bufopts)
+    vim.keymap.set('i', '<C-f>', vim.lsp.buf.format, bufopts)
   end
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local null_ls = require("null-ls")
+local null_ls = require('null-ls')
 
 null_ls.setup({
   sources = {
@@ -61,14 +62,14 @@ null_ls.setup({
     null_ls.builtins.code_actions.cspell,
     null_ls.builtins.diagnostics.cspell.with({
       diagnostics_postprocess = function(diagnostic)
-        diagnostic.severity = vim.diagnostic.severity["HINT"]
+        diagnostic.severity = vim.diagnostic.severity['HINT']
       end,
       diagnostic_config = {
         underline = true,
         virtual_text = false,
         signs = false,
       },
-    })
+    }),
   },
 })
 
@@ -80,15 +81,15 @@ lspconfig.sumneko_lua.setup {
   on_attach = on_attach,
   settings = {
     Lua = {
-      diagnostics = { globals = { "vim", "hs" } },
+      diagnostics = { globals = { 'vim', 'hs' } },
       workspace = {
         library = {
-          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          [vim.fn.stdpath("config") .. "/lua"] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.stdpath('config') .. '/lua'] = true,
         },
       },
     },
-  }
+  },
 }
 
 lspconfig.tsserver.setup {
@@ -98,7 +99,7 @@ lspconfig.tsserver.setup {
     diagnostics = {
       ignoredCodes = { 7016, 80001, 80006, 80007 },
     },
-  }
+  },
 }
 
 lspconfig.yamlls.setup {
@@ -107,10 +108,10 @@ lspconfig.yamlls.setup {
   settings = {
     yaml = {
       schemas = {
-        ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = ".gitlab-ci.yml",
+        ['https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json'] = '.gitlab-ci.yml',
       },
     },
-  }
+  },
 }
 
 lspconfig.jsonls.setup {
@@ -157,10 +158,10 @@ lspconfig.r_language_server.setup {
   settings = {
     r = {
       lsp = {
-        diagnostics = false
-      }
-    }
-  }
+        diagnostics = false,
+      },
+    },
+  },
 }
 
 lspconfig.lemminx.setup {
@@ -173,14 +174,14 @@ lspconfig.eslint.setup {
   on_attach = function(client, bufnr)
     on_attach(client, bufnr)
 
-    local group = vim.api.nvim_create_augroup("FixIt", { clear = true })
+    local group = vim.api.nvim_create_augroup('FixIt', { clear = true })
 
-    vim.api.nvim_create_autocmd("BufWritePre", {
+    vim.api.nvim_create_autocmd('BufWritePre', {
       buffer = bufnr,
       group = group,
       callback = function()
         vim.schedule(function()
-          vim.cmd("EslintFixAll")
+          vim.cmd('EslintFixAll')
         end)
       end,
     })
