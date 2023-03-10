3e46f08ad487add22097f156e8bb1c73e8b2719e
Darrin Holst <darrinholst@gmail.com>
2023-03-09 18:32:35 -0600

    🤷

--- .config/nvim/after/plugin/tree.lua
+++ .config/nvim/after/plugin/tree.lua
@@ -1,63 +1,69 @@
-local lib = require("nvim-tree.lib")
-local view = require("nvim-tree.view")
+local lib = require('nvim-tree.lib')
+local view = require('nvim-tree.view')
 
 local function collapse_all()
-  require("nvim-tree.actions.tree-modifiers.collapse-all").fn()
+  require('nvim-tree.actions.tree-modifiers.collapse-all').fn()
 end
 
 local function edit_or_open()
-  local action = "edit"
+  local action = 'edit'
   local node = lib.get_node_at_cursor()
 
   if node.link_to and not node.nodes then
-    require("nvim-tree.actions.node.open-file").fn(action, node.link_to)
+    require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
     view.close() -- Close the tree if file was opened
   elseif node.nodes ~= nil then
     lib.expand_or_collapse(node)
   else
-    require("nvim-tree.actions.node.open-file").fn(action, node.absolute_path)
+    require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
     view.close()
   end
 end
 
 local function vsplit_preview()
-  local action = "vsplit"
+  local action = 'vsplit'
   local node = lib.get_node_at_cursor()
 
   if node.link_to and not node.nodes then
-    require("nvim-tree.actions.node.open-file").fn(action, node.link_to)
+    require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
   elseif node.nodes ~= nil then
     lib.expand_or_collapse(node)
   else
-    require("nvim-tree.actions.node.open-file").fn(action, node.absolute_path)
+    require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
   end
 
   view.focus()
 end
 
-require("nvim-tree").setup({
+require('nvim-tree').setup({
   git = { enable = true, ignore = false },
+  update_focused_file = {
+    enable = false,
+  },
   view = {
     adaptive_size = true,
     mappings = {
       list = {
         -- { key = "l", action = "edit", action_cb = edit_or_open },
         -- { key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
-        { key = "h", action = "close_node" },
+        { key = 'h', action = 'close_node' },
         -- { key = "H", action = "collapse_all", action_cb = collapse_all },
-        { key = { "<C-g>" }, action = "grep here", action_cb = function(node)
-          require("telescope.builtin").live_grep({ search_dirs = { node.absolute_path } })
-        end }
-      }
-    }
+        {
+          key = { '<C-g>' },
+          action = 'grep here',
+          action_cb = function(node)
+            require('telescope.builtin').live_grep({ search_dirs = { node.absolute_path } })
+          end,
+        },
+      },
+    },
   },
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-  group = vim.api.nvim_create_augroup("NvimTree", { clear = true }),
-  pattern = "NvimTree",
-
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('NvimTree', { clear = true }),
+  pattern = 'NvimTree',
   callback = function()
     vim.diagnostic.disable(0)
-  end
+  end,
 })
--- .config/nvim/lua/my/settings.lua
+++ .config/nvim/lua/my/settings.lua
@@ -1,26 +1,27 @@
 vim.g.mapleader        = ','
 vim.opt.cursorline     = true --  highlight current line
+vim.opt.cmdheight      = 2
 vim.opt.expandtab      = true --  spaces, not tabs
 vim.opt.guifont        = 'Fira Code Retina Nerd Font Complete:h17'
 vim.opt.ignorecase     = true --  ignore case
-vim.opt.laststatus     = 3 --     show the nvim global status line
+vim.opt.laststatus     = 3    --     show the nvim global status line
 vim.opt.swapfile       = false
 vim.opt.backup         = false
 vim.opt.number         = true
 vim.opt.relativenumber = false
-vim.opt.scrolloff      = 5 --     Start scrolling 5 lines away from margins
-vim.opt.shiftwidth     = 2 --     autoindent == 2 spaces
-vim.opt.sidescrolloff  = 10 --    Start scrolling 10 columns away from margins
+vim.opt.scrolloff      = 5    --     Start scrolling 5 lines away from margins
+vim.opt.shiftwidth     = 2    --     autoindent == 2 spaces
+vim.opt.sidescrolloff  = 10   --    Start scrolling 10 columns away from margins
 vim.opt.signcolumn     = 'yes'
 vim.opt.smartcase      = true --  don't ignore case if there's an uppercase character
-vim.opt.softtabstop    = 2 --     don't use <Tab>s
-vim.opt.tabstop        = 2 --     tab == 2 spaces
+vim.opt.softtabstop    = 2    --     don't use <Tab>s
+vim.opt.tabstop        = 2    --     tab == 2 spaces
 vim.opt.termguicolors  = true
 vim.opt.timeoutlen     = 1000
 vim.opt.ttimeoutlen    = 100
-vim.opt.updatetime     = 300 --   Smaller updatetime for CursorHold & CursorHoldI
+vim.opt.updatetime     = 300   --   Smaller updatetime for CursorHold & CursorHoldI
 -- vim.opt.inccommand     = 'split' -- show the effects of a search / replace in a live preview window
-vim.opt.vb             = true --  no beeping
+vim.opt.vb             = true  --  no beeping
 vim.opt.wrap           = false -- no line wrapping by default
 vim.opt.hlsearch       = true
 vim.wo.foldlevel       = 99
