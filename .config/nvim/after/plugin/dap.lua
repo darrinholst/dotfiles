aedc8b686066ac696d757812e46c443f8de9154a
Darrin Holst <darrinholst@gmail.com>
2023-03-24 07:44:40 -0500

    latest

--- .config/nvim/after/plugin/dap.lua
+++ .config/nvim/after/plugin/dap.lua
@@ -1,18 +1,17 @@
-local dap = require("dap")
-require("dapui").setup()
+local dap = require('dap')
+require('dapui').setup()
 
 vim.fn.sign_define('DapBreakpoint', { text = '🔴', texthl = '', linehl = '', numhl = '' })
 vim.fn.sign_define('DapBreakpointRejected', { text = '❓', texthl = '', linehl = '', numhl = '' })
 
 dap.adapters.node = {
-  type = "executable",
-  command = "node",
-  args = { os.getenv("HOME") .. "/.local/share/node-debug2/out/src/nodeDebug.js" },
+  type = 'executable',
+  command = os.getenv('HOME') .. '/.local/share/nvim/mason/bin/node-debug2-adapter'
 }
 
 dap.adapters.sh = {
-  type = "executable",
-  command = "bash-debug-adapter",
+  type = 'executable',
+  command = 'bash-debug-adapter',
 }
 
 dap.configurations.sh = {
@@ -21,50 +20,50 @@ dap.configurations.sh = {
     argsString = '',
     cwd = vim.fn.getcwd(),
     env = {},
-    name = "Launch",
-    pathBash = "/opt/homebrew/bin/bash",
-    pathBashdb = "/opt/homebrew/bin/bashdb",
-    pathBashdbLib = "/opt/homebrew/share/bashdb/",
-    pathCat = "/bin/cat",
-    pathMkfifo = "/usr/bin/mkfifo",
-    pathPkill = "/usr/bin/pkill",
-    program = "${file}",
-    request = "launch",
+    name = 'Launch',
+    pathBash = '/opt/homebrew/bin/bash',
+    pathBashdb = '/opt/homebrew/bin/bashdb',
+    pathBashdbLib = '/opt/homebrew/share/bashdb/',
+    pathCat = '/bin/cat',
+    pathMkfifo = '/usr/bin/mkfifo',
+    pathPkill = '/usr/bin/pkill',
+    program = '${file}',
+    request = 'launch',
     showDebugOutput = false,
-    terminalKind = "integrated",
+    terminalKind = 'integrated',
     trace = true,
-    type = "sh",
-  }
+    type = 'sh',
+  },
 }
 
 dap.configurations.javascript = {
   {
-    name = "Run script",
-    type = "node",
-    request = "launch",
-    runtimeExecutable = "npm",
+    name = 'Run script',
+    type = 'node',
+    request = 'launch',
+    runtimeExecutable = 'npm',
     runtimeArgs = function()
-      return { "run-script", vim.fn.input("Which script: ") }
+      return { 'run-script', vim.fn.input('Which script: ') }
     end,
     cwd = vim.fn.getcwd(),
     sourceMaps = true,
-    protocol = "inspector",
-    console = "integratedTerminal",
+    protocol = 'inspector',
+    console = 'integratedTerminal',
   },
   {
-    name = "Launch",
-    type = "node",
-    request = "launch",
-    program = "${file}",
+    name = 'Launch',
+    type = 'node',
+    request = 'launch',
+    program = '${file}',
     cwd = vim.fn.getcwd(),
     sourceMaps = true,
-    protocol = "inspector",
-    console = "integratedTerminal",
+    protocol = 'inspector',
+    console = 'integratedTerminal',
   },
   {
-    name = "Attach to process",
-    type = "node",
-    request = "attach",
-    processId = require "dap.utils".pick_process,
+    name = 'Attach to process',
+    type = 'node',
+    request = 'attach',
+    processId = require 'dap.utils'.pick_process,
   },
 }
