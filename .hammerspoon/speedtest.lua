aa5504e270209d540e005f3635009ee99a979fb8
Darrin Holst <darrinholst@gmail.com>
2023-04-18 17:19:49 -0500

    speedtest v2

--- .hammerspoon/speedtest.lua
+++ .hammerspoon/speedtest.lua
@@ -1,11 +1,15 @@
 local main = nil
 SPEEDTEST_MENU = hs.menubar.new()
 
+local function mbps(n)
+  return string.format("%.0f", n * 8 / 1000000)
+end
+
 main = function()
   local results = hs.execute("/opt/homebrew/bin/speedtest -f json")
   local json = hs.json.decode(results)
-  print(hs.inspect(json))
-  SPEEDTEST_MENU:setTitle(json.isp .. string.format(": %.0f", json.download.bandwidth * 8 / 1000000))
+  local isp = json.isp:match("%S+")
+  SPEEDTEST_MENU:setTitle(isp .. ": " .. mbps(json.download.bandwidth) .. "↓ " .. mbps(json.upload.bandwidth) .. "↑")
 end
 
 SPEEDTEST_TIMER = hs.timer.new(300, main)
