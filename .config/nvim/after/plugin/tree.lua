70adf57e61c7e5bccefe5b8feb4aaf1a71fd4c07
Darrin Holst <darrinholst@gmail.com>
2023-04-25 11:13:53 -0500

    🌲

--- .config/nvim/after/plugin/tree.lua
+++ .config/nvim/after/plugin/tree.lua
@@ -1,63 +1,62 @@
-local lib = require('nvim-tree.lib')
-local view = require('nvim-tree.view')
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
 
-local function collapse_all()
-  require('nvim-tree.actions.tree-modifiers.collapse-all').fn()
-end
-
-local function edit_or_open()
-  local action = 'edit'
-  local node = lib.get_node_at_cursor()
-
-  if node.link_to and not node.nodes then
-    require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
-    view.close() -- Close the tree if file was opened
-  elseif node.nodes ~= nil then
-    lib.expand_or_collapse(node)
-  else
-    require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
-    view.close()
-  end
-end
-
-local function vsplit_preview()
-  local action = 'vsplit'
-  local node = lib.get_node_at_cursor()
-
-  if node.link_to and not node.nodes then
-    require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
-  elseif node.nodes ~= nil then
-    lib.expand_or_collapse(node)
-  else
-    require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
   end
 
-  view.focus()
+  vim.keymap.set('n', '<C-]>', api.tree.change_root_to_node, opts('CD'))
+  vim.keymap.set('n', '<C-k>', api.node.show_info_popup, opts('Info'))
+  vim.keymap.set('n', '<C-r>', api.fs.rename_sub, opts('Rename: Omit Filename'))
+  vim.keymap.set('n', '<C-v>', api.node.open.vertical, opts('Open: Vertical Split'))
+  vim.keymap.set('n', '<C-x>', api.node.open.horizontal, opts('Open: Horizontal Split'))
+  vim.keymap.set('n', '<BS>', api.node.navigate.parent_close, opts('Close Directory'))
+  vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', '<Tab>', api.node.open.preview, opts('Open Preview'))
+  vim.keymap.set('n', '.', api.node.run.cmd, opts('Run Command'))
+  vim.keymap.set('n', '-', api.tree.change_root_to_parent, opts('Up'))
+  vim.keymap.set('n', 'a', api.fs.create, opts('Create'))
+  vim.keymap.set('n', 'B', api.tree.toggle_no_buffer_filter, opts('Toggle No Buffer'))
+  vim.keymap.set('n', 'c', api.fs.copy.node, opts('Copy'))
+  vim.keymap.set('n', 'C', api.tree.toggle_git_clean_filter, opts('Toggle Git Clean'))
+  vim.keymap.set('n', '[c', api.node.navigate.git.prev, opts('Prev Git'))
+  vim.keymap.set('n', ']c', api.node.navigate.git.next, opts('Next Git'))
+  vim.keymap.set('n', 'd', api.fs.remove, opts('Delete'))
+  vim.keymap.set('n', 'D', api.fs.trash, opts('Trash'))
+  vim.keymap.set('n', 'E', api.tree.expand_all, opts('Expand All'))
+  vim.keymap.set('n', 'e', api.fs.rename_basename, opts('Rename: Basename'))
+  vim.keymap.set('n', ']e', api.node.navigate.diagnostics.next, opts('Next Diagnostic'))
+  vim.keymap.set('n', '[e', api.node.navigate.diagnostics.prev, opts('Prev Diagnostic'))
+  vim.keymap.set('n', 'g?', api.tree.toggle_help, opts('Help'))
+  vim.keymap.set('n', 'gy', api.fs.copy.absolute_path, opts('Copy Absolute Path'))
+  vim.keymap.set('n', 'H', api.tree.toggle_hidden_filter, opts('Toggle Dotfiles'))
+  vim.keymap.set('n', 'I', api.tree.toggle_gitignore_filter, opts('Toggle Git Ignore'))
+  vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'O', api.node.open.no_window_picker, opts('Open: No Window Picker'))
+  vim.keymap.set('n', 'p', api.fs.paste, opts('Paste'))
+  vim.keymap.set('n', 'P', api.node.navigate.parent, opts('Parent Directory'))
+  vim.keymap.set('n', 'q', api.tree.close, opts('Close'))
+  vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
+  vim.keymap.set('n', 'R', api.tree.reload, opts('Refresh'))
+  vim.keymap.set('n', 's', api.node.run.system, opts('Run System'))
+  vim.keymap.set('n', 'S', api.tree.search_node, opts('Search'))
+  vim.keymap.set('n', 'W', api.tree.collapse_all, opts('Collapse'))
+  vim.keymap.set('n', 'x', api.fs.cut, opts('Cut'))
+  vim.keymap.set('n', 'y', api.fs.copy.filename, opts('Copy Name'))
+  vim.keymap.set('n', 'Y', api.fs.copy.relative_path, opts('Copy Relative Path'))
+  vim.keymap.set('n', '<2-LeftMouse>', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', '<2-RightMouse>', api.tree.change_root_to_node, opts('CD'))
+  vim.keymap.set('n', '<C-g>', function()
+    local node = api.tree.get_node_under_cursor()
+    require('telescope.builtin').live_grep({ search_dirs = { node.absolute_path } })
+  end, opts('grep here'))
 end
 
 require('nvim-tree').setup({
+  on_attach = on_attach,
   git = { enable = true, ignore = false },
-  update_focused_file = {
-    enable = false,
-  },
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        -- { key = "l", action = "edit", action_cb = edit_or_open },
-        -- { key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
-        { key = 'h', action = 'close_node' },
-        -- { key = "H", action = "collapse_all", action_cb = collapse_all },
-        {
-          key = { '<C-g>' },
-          action = 'grep here',
-          action_cb = function(node)
-            require('telescope.builtin').live_grep({ search_dirs = { node.absolute_path } })
-          end,
-        },
-      },
-    },
-  },
+  update_focused_file = { enable = false },
+  view = { adaptive_size = true },
 })
 
 vim.api.nvim_create_autocmd('FileType', {
