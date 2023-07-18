25205fbcef34b1d4ab2f9497102edabdbe392ee2
Darrin Holst <darrinholst@gmail.com>
2023-07-18 15:52:00 -0500

    neoooooooooooooooooooooooooooootree

--- .config/nvim/after/plugin/neotree.lua
+++ .config/nvim/after/plugin/neotree.lua
@@ -16,14 +16,6 @@ require('neo-tree').setup({
   enable_normal_mode_for_inputs = false,                             -- Enable normal mode for input dialogs.
   open_files_do_not_replace_types = { 'terminal', 'trouble', 'qf' }, -- when opening files, do not use windows containing these filetypes or buftypes
   sort_case_insensitive = false,                                     -- used when sorting files and directories in the tree
-  sort_function = nil,                                               -- use a custom function for sorting files and directories in the tree
-  -- sort_function = function (a,b)
-  --       if a.type == b.type then
-  --           return a.path > b.path
-  --       else
-  --           return a.type > b.type
-  --       end
-  --   end , -- this sorts files and directories descendantly
   default_component_configs = {
     container = {
       enable_character_fade = true,
@@ -31,12 +23,10 @@ require('neo-tree').setup({
     indent = {
       indent_size = 2,
       padding = 1, -- extra padding on left hand side
-      -- indent guides
       with_markers = true,
       indent_marker = '│',
       last_indent_marker = '└',
       highlight = 'NeoTreeIndentMarker',
-      -- expander config, needed for nesting files
       with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
       expander_collapsed = '',
       expander_expanded = '',
@@ -46,8 +36,6 @@ require('neo-tree').setup({
       folder_closed = '',
       folder_open = '',
       folder_empty = '󰜌',
-      -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
-      -- then these will never be used.
       default = '*',
       highlight = 'NeoTreeFileIcon',
     },
@@ -62,12 +50,10 @@ require('neo-tree').setup({
     },
     git_status = {
       symbols = {
-        -- Change type
-        added     = '✚', -- or "✚", but this is redundant info if you use git_status_colors on the name
-        modified  = '', -- or "", but this is redundant info if you use git_status_colors on the name
-        deleted   = '✖', -- this can only be used in the git_status source
-        renamed   = '󰁕', -- this can only be used in the git_status source
-        -- Status type
+        added     = '✚',
+        modified  = '',
+        deleted   = '✖',
+        renamed   = '󰁕',
         untracked = '',
         ignored   = '',
         unstaged  = '󰄱',
@@ -76,10 +62,6 @@ require('neo-tree').setup({
       },
     },
   },
-  -- A list of functions, each representing a global custom command
-  -- that will be available in all sources (if not overridden in `opts[source_name].commands`)
-  -- see `:h neo-tree-custom-commands-global`
-  commands = {},
   window = {
     position = 'left',
     width = 40,
@@ -99,38 +81,14 @@ require('neo-tree').setup({
       ['l'] = 'focus_preview',
       ['S'] = 'open_split',
       ['s'] = 'open_vsplit',
-      -- ["S"] = "split_with_window_picker",
-      -- ["s"] = "vsplit_with_window_picker",
       ['t'] = 'open_tabnew',
-      -- ["<cr>"] = "open_drop",
-      -- ["t"] = "open_tab_drop",
-      ['w'] = 'open_with_window_picker',
-      --["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
-      ['C'] = 'close_node',
-      -- ['C'] = 'close_all_subnodes',
       ['z'] = 'close_all_nodes',
-      --["Z"] = "expand_all_nodes",
-      ['a'] = {
-        'add',
-        -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
-        -- some commands may take optional config options, see `:h neo-tree-mappings` for details
-        config = {
-          show_path = 'none', -- "none", "relative", "absolute"
-        },
-      },
-      ['A'] = 'add_directory', -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+      ['a'] = { 'add', config = { show_path = 'none' } },
       ['d'] = 'delete',
       ['r'] = 'rename',
-      ['y'] = 'copy_to_clipboard',
+      ['c'] = 'copy_to_clipboard',
       ['x'] = 'cut_to_clipboard',
       ['p'] = 'paste_from_clipboard',
-      ['c'] = 'copy', -- takes text input for destination, also accepts the optional config.show_path option like "add":
-      -- ["c"] = {
-      --  "copy",
-      --  config = {
-      --    show_path = "none" -- "none", "relative", "absolute"
-      --  }
-      --}
       ['m'] = 'move', -- takes text input for destination, also accepts the optional config.show_path option like "add".
       ['q'] = 'close_window',
       ['R'] = 'refresh',
