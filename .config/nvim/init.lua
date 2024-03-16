d30d7a3d0b553891d922e1f7c139757d21990e5a
Darrin Holst <darrinholst@gmail.com>
2024-03-16 10:07:18 -0500

    🤷

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,16 +1,14 @@
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("my.settings")
-
-require('lazy').setup('my.plugins')
-
+require("lazy").setup("my.plugins")
 require("my.keymaps")
 require("my.autocommands")
 require("my.highlights")
