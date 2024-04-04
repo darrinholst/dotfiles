ae3b68eedbe377159932c13a8bc9cbbf299ac234
Darrin Holst <darrinholst@gmail.com>
2024-04-04 06:13:35 -0500

    Replace filetype autocommands with ftplugin

--- .config/nvim/lua/my/autocommands.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-  callback = function()
-    vim.opt_local.colorcolumn = "81"
-  end,
-})
