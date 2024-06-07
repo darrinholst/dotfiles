d3b0a97e5191712aaa532dd51e50c65b779f2d86
Darrin Holst <darrinholst@gmail.com>
2024-06-07 14:59:36 -0500

    yada yada yada

--- .config/nvim/lua/my/plugins/lsp.lua
+++ .config/nvim/lua/my/plugins/lsp.lua
@@ -15,8 +15,8 @@ return {
           vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
         end
 
-        map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-        map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+        map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+        map("gr", vim.lsp.buf.references, "[G]oto [R]eferences")
         map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
         map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
         map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
--- sso-auto-approve/approve.js
+++ sso-auto-approve/approve.js
@@ -1,3 +1,4 @@
 document.getElementById('cli_login_button')?.click();
 document.getElementById('cli_verification_btn')?.click();
+document.querySelector('button[data-testid="allow-access-button"]')?.click();
 
