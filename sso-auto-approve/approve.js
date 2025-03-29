9209b67b6748b3851c82ec2401f84ac78b120bf9
Darrin Holst <darrinholst@gmail.com>
2025-03-29 11:06:01 -0500

    things my intern changed for me

--- sso-auto-approve/approve.js
+++ sso-auto-approve/approve.js
@@ -1,4 +1,16 @@
-document.getElementById('cli_login_button')?.click();
-document.getElementById('cli_verification_btn')?.click();
-document.querySelector('button[data-testid="allow-access-button"]')?.click();
+console.log('SSO Auto Approve: Running on', window.location.href);
 
+setInterval(() => clickButton('Confirm and continue', 'Allow') , 500);
+
+function clickButton(...buttonsToClick) {
+  const buttons = Array.from(document.querySelectorAll('button'));
+  const element = buttons.find(btn => buttonsToClick.includes(btn.textContent))
+
+  if (element) {
+    console.log('SSO Auto Approve: Clicking button with text', element.textContent);
+    element.click();
+    return true;
+  }
+
+  return false;
+}
