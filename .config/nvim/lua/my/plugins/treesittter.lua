dbc0b511dfb37bfee22ef5b4099d49f809032c87
Darrin Holst <darrinholst@gmail.com>
2026-04-14 10:42:25 -0500

    I'm sure it's fine

--- .config/nvim/lua/my/plugins/treesittter.lua
+++ .config/nvim/lua/my/plugins/treesittter.lua
@@ -1,52 +1,83 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
   dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
+    { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
     "nvim-treesitter/nvim-treesitter-context",
   },
   config = function()
-    require("nvim-treesitter.configs").setup {
-      modules = {},
-      ignore_install = { "swift" },
-      ensure_installed = {
-        "bash",
-        "html",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "vim",
-        "vimdoc",
-        "javascript",
-        "json",
-      },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-      fold = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<CR>",
-          node_incremental = "<CR>",
-          scope_incremental = "<S-CR>",
-          node_decremental = "<BS>",
-        },
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
+    require("nvim-treesitter").setup()
 
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-          },
-        },
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
+    })
+
+    require("nvim-treesitter").install({
+      "bash",
+      "html",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "vim",
+      "vimdoc",
+      "javascript",
+      "json",
+      "typescript",
+      "tsx",
+    })
+
+    require("nvim-treesitter-textobjects").setup {
+      select = {
+        lookahead = true,
       },
-      sync_install = false,
     }
 
+    vim.keymap.set({ "x", "o" }, "af", function()
+      require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+    end)
+    vim.keymap.set({ "x", "o" }, "if", function()
+      require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+    end)
+
     require("treesitter-context").setup()
+
+    -- incremental selection using treesitter nodes (replaces old incremental_selection module)
+    local selection_stack = {}
+
+    local function select_node(node)
+      local sr, sc, er, ec = node:range()
+      vim.fn.setpos("'<", { 0, sr + 1, sc + 1, 0 })
+      vim.fn.setpos("'>", { 0, er + 1, ec, 0 })
+      vim.cmd("normal! gv")
+    end
+
+    vim.keymap.set("n", "<CR>", function()
+      local node = vim.treesitter.get_node()
+      if not node then return end
+      selection_stack = { node }
+      select_node(node)
+    end, { desc = "Init treesitter incremental selection" })
+
+    vim.keymap.set("x", "<CR>", function()
+      local current = selection_stack[#selection_stack]
+      if not current then return end
+      local parent = current:parent()
+      if not parent then return end
+      table.insert(selection_stack, parent)
+      select_node(parent)
+    end, { desc = "Increment treesitter selection" })
+
+    vim.keymap.set("x", "<BS>", function()
+      if #selection_stack <= 1 then return end
+      table.remove(selection_stack)
+      local node = selection_stack[#selection_stack]
+      if node then
+        select_node(node)
+      end
+    end, { desc = "Decrement treesitter selection" })
   end,
 }
